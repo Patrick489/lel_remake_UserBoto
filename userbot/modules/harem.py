5bd0f9243448ea71c7cb0ca6c7fc908f3fab9ee8
Nao tomori <73517900+Neko-Chan1@users.noreply.github.com>
2021-02-09 11:18:58 +0800

    Update harem.py

--- userbot/modules/harem.py
+++ userbot/modules/harem.py
@@ -8,7 +8,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern='^s|S(?: |$)(.*)')
+@register(outgoing=True, pattern='^S|s(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
     sleep(0)
