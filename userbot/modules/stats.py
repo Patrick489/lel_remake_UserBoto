acdfc0c36c2d19ac5b6b13621839cd58939f1524
Nao tomori <73517900+Neko-Chan1@users.noreply.github.com>
2021-03-18 18:48:10 +0800

    Create stats.py

---
+++ userbot/modules/stats.py
@@ -0,0 +1,44 @@
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+import io
+from userbot import bot, CMD_HELP
+from userbot.events import register
+
+@register(outgoing=True, pattern=r"^\.ustat")
+async def _(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        return await event.edit("```Balas di Pesan Goblok!!.```")
+    reply_message = await event.get_reply_message()
+    if not reply_message.text:
+        return await event.edit("```Balas di Pesan Goblok!!```")
+    chat = "@tgscanrobot"
+    await event.edit("Checking....")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=1557162396))
+            msg = await event.client.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("unblock me @tgscanrobot to work")
+            return
+        if response.text.startswith("I understand only text"):
+            await event.edit("Sorry i cant't check group this user **BURIK!!**")
+        else:
+            if response.text.startswith("Information"):
+                response = conv.wait_event(
+                    events.NewMessage(
+                        incoming=True,
+                        from_users=1557162396))
+                response = await response
+                await event.delete()
+                await event.client.send_message(event.chat_id, response.message, reply_to=reply_message.id)
+                await event.client.delete_messages(conv.chat_id,
+                                                   [msg.id, response.id])
+            else:
+                await event.edit("try again")
+        await bot.send_read_acknowledge(conv.chat_id)
