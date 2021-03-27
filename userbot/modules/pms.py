16acfe668c6c5f7b50016ce8ed4dbead87be96dc
Patrick489 <78151815+Patrick489@users.noreply.github.com>
2021-03-27 09:43:58 +0700

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -28,7 +28,7 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 DEF_UNAPPROVED_MSG = (
     "╭━━━━━━━━━━━━━━━━━━━━━╮\n"
     "     SELAMAT DATANG DI\n"
-    f"         `{DEFAULTUSER}` CHAT\n"
+    f"        `{DEFAULTUSER}` CHAT\n"
     "╰━━━━━━━━━━━━━━━━━━━━━╯\n\n"
 
     f"  Saya Bot Assisten `{DEFAULTUSER}`\n"
