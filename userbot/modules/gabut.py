95ad991bf358d008400390cf25445f1635e778c7
Patrick489 <78151815+Patrick489@users.noreply.github.com>
2021-04-08 22:26:57 +0700

    Update gabut.py

--- userbot/modules/gabut.py
+++ userbot/modules/gabut.py
@@ -752,6 +752,18 @@ async def typewriter(typew):
     await typew.edit("`Tapi Bo'ong`")
 # Create by myself @localheart
 
+
+@register(outgoing=True, pattern='^.ange(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(3)
+    await typew.edit("`Ayanggggg😖`")
+    sleep(3)
+    await typew.edit("`Ange😫`")
+    sleep(1)
+    await typew.edit("`Ayukkk Ewean Ayanggg🤤`")
+
+
 CMD_HELP.update({
     "gabut":
     "`.gabut` ; `.dino`\
