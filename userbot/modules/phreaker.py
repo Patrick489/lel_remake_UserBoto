4e6c3f4d5d9f18bea370e087b0fc46aa2ef69638
Patrick489 <78151815+Patrick489@users.noreply.github.com>
2021-04-13 11:26:34 +0700

    Update phreaker.py

--- userbot/modules/phreaker.py
+++ userbot/modules/phreaker.py
@@ -22,7 +22,7 @@ async def _(event):
     async with bot.conversation("@Carol5_bot") as conv:
         try:
             jemboed = await conv.send_message(f"/gen {query}")
-            await asyncio.sleep(8)
+            await asyncio.sleep(10)
             asu = await conv.get_response()
             await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
